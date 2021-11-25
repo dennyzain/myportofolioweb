0398d35eb2c1bd7d6f3b4da64255e17d6ace1484
dennyabbaszain <abbasdenny24@gmail.com>
2021-11-25 11:23:11 +0700

    initialcommit

--- pages/index.js
+++ pages/index.js
@@ -51,16 +51,30 @@ export default function Home() {
   return (
     <>
       {' '}
-      <ediv className="h-full w-full top-0 left-0 fixed overflow-hidden ">
+      <div className="h-full w-full top-0 left-0 fixed overflow-hidden ">
         <Header />
         <div ref={scrollContainer}>
-          <Hero />
-          <About />
-          <Portofolio />
-          <Contacts />
-          <Footer />
+          {images.map((image, index) => (
+            <>
+              <div key={index} className="m-20 ">
+                <img src={image} alt={`people ${index}`} />
+              </div>
+              <h2>
+                Skew <span className="m-4">Scrolling</span>
+              </h2>
+            </>
+          ))}
         </div>
-      </ediv>
+      </div>
     </>
   );
 }
+
+const images = [
+  'https://images.unsplash.com/photo-1551392505-f4056032826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1497&q=80',
+  'https://images.unsplash.com/photo-1594312180721-3b5217cfc65f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+  'https://images.unsplash.com/photo-1593806812862-1dc510b769a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80',
+  'https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+  'https://images.unsplash.com/photo-1484517186945-df8151a1a871?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80',
+  'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+];
