58e1a5a61172fc85f413cb8de2aef205eb2327df
dennyabbaszain <abbasdenny24@gmail.com>
2021-11-27 13:10:05 +0700

    initialcommit

--- pages/index.js
+++ pages/index.js
@@ -40,9 +40,9 @@ export default function Home() {
     const difference = data.current - data.rounded;
     const acceleration = difference / width;
     const velocity = +acceleration;
-    const skew = velocity * 7.5;
+    const skew = velocity * 2.5;
     //Assign skew and smooth scrolling to the scroll container
-    scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+    scrollContainer.current.style.transform = `translateY(-${data.rounded}px) skewY(${skew}deg)`;
 
     //loop vai raf
     window.requestAnimationFrame(() => skewScrolling());
