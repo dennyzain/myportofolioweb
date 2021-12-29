7563d42cc66534ace95c2a0ffc28f3433055bbbf
dennyzain <abbasdenny24@gmail.com>
2021-12-29 08:44:17 +0700

    delete smooth scroll

--- components/molecules/Loading.js
+++ components/molecules/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <p className="text-lg">LOADING</p>
+      <p className="text-lg animate-bounce">LOADING</p>
     </div>
   );
 }
